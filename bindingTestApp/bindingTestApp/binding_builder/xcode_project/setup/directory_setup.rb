931e8c5becf74120dbff78504ac9c4c8f343939b
木村太一朗 <kimura@tanosys.com>
2025-07-29 11:06:09 +0900

    　リファクタ

--- bindingTestApp/bindingTestApp/binding_builder/xcode_project/setup/directory_setup.rb
+++ bindingTestApp/bindingTestApp/binding_builder/xcode_project/setup/directory_setup.rb
@@ -50,22 +50,7 @@ class DirectorySetup < PbxprojManager
       create_core_files_if_needed(directories_to_create)
     end
     
-    # ライブラリパッケージを追加
-    setup_libraries
-    
-    # HotLoader機能をAppDelegateに追加
-    add_hotloader_to_app_delegate
-    
-    # HotLoad Build Phase設定
-    setup_hotload_build_phase
-    
-    # Info.plistからStoryBoard参照を削除
-    remove_storyboard_from_info_plist
-    
-    # membershipExceptionsを設定
-    setup_membership_exceptions
-    
-    puts "Directory setup completed successfully!"
+    puts "Directory creation completed successfully!"
   end
 
   private
@@ -105,69 +90,6 @@ class DirectorySetup < PbxprojManager
   end
 
 
-  def setup_libraries
-    require_relative 'library_setup'
-    
-    library_setup = LibrarySetup.new(@project_file_path)
-    library_setup.setup_libraries
-  end
-
-  def add_hotloader_to_app_delegate
-    require_relative 'app_delegate_setup'
-    
-    app_delegate_setup = AppDelegateSetup.new(@project_file_path)
-    app_delegate_setup.add_hotloader_functionality
-  end
-
-  def setup_hotload_build_phase
-    require_relative 'hotload_setup'
-    
-    puts "Setting up HotLoad Build Phase..."
-    hotload_setup = HotLoadSetup.new(@project_file_path)
-    hotload_setup.setup_hotload_build_phase
-  end
-
-  def remove_storyboard_from_info_plist
-    puts "Removing StoryBoard references from Info.plist..."
-    
-    # Info.plistファイルを探す
-    project_dir = File.dirname(File.dirname(@project_file_path))
-    info_plist_path = find_info_plist_file(project_dir)
-    
-    if info_plist_path.nil?
-      puts "Warning: Could not find Info.plist file. StoryBoard references not removed."
-      return
-    end
-
-    puts "Updating Info.plist: #{info_plist_path}"
-    
-    # Info.plistの内容を読み込む
-    content = File.read(info_plist_path)
-    
-    # 既にStoryBoard参照が削除されているかチェック
-    unless content.include?("UISceneStoryboardFile")
-      puts "StoryBoard references already removed from Info.plist"
-      return
-    end
-    
-    # StoryBoard参照を削除
-    updated_content = remove_storyboard_references(content)
-    
-    # ファイルに書き戻す
-    File.write(info_plist_path, updated_content)
-    puts "StoryBoard references removed from Info.plist successfully"
-  end
-
-  def find_info_plist_file(project_dir)
-    # プロジェクトディレクトリから再帰的にInfo.plistを検索
-    Dir.glob("#{project_dir}/**/Info.plist").first
-  end
-
-  def remove_storyboard_references(content)
-    # UISceneStoryboardFileキーとその値を削除
-    content = content.gsub(/\s*<key>UISceneStoryboardFile<\/key>\s*\n\s*<string>.*?<\/string>\s*\n/, "")
-    content
-  end
 
   def create_core_files_if_needed(directories_to_create)
     core_created = directories_to_create.any? { |dir| dir[:name] == "Core" }
@@ -234,16 +156,3 @@ class DirectorySetup < PbxprojManager
   end
 end
 
-# コマンドライン実行
-if __FILE__ == $0
-  begin
-    # binding_builderディレクトリから検索開始
-    binding_builder_dir = File.expand_path("../../", __FILE__)
-    project_file_path = ProjectFinder.find_project_file(binding_builder_dir)
-    setup = DirectorySetup.new(project_file_path)
-    setup.create_missing_directories
-  rescue => e
-    puts "Error: #{e.message}"
-    exit 1
-  end
-end

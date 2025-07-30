2a30e6843ce9d65b2605da0b199eb339560e9883
木村太一朗 <kimura@tanosys.com>
2025-07-31 01:19:13 +0900

    スクリプト修正

--- bindingTestApp/clear_and_setup.sh
+++ bindingTestApp/clear_and_setup.sh
@@ -10,9 +10,11 @@ rm -rf ~/Library/Developer/Xcode/DerivedData
 rm -rf .build
 
 cd ~/resource/bindingTestApp/bindingTestApp/bindingTestApp
+rm -rf binding_builder
+rm -rf hot_loader
 cp -r ~/resource/SwiftJsonUI/installer .
 cd installer
-./install_sjui.sh -v 6.1.0-alpha --skip-bundle
+./install_sjui.sh -v 6.1.0 --skip-bundle
 cd ../binding_builder/
 ./sjui setup
 ./sjui g view splash --root

d3c1112ee9ea2d648ec5f98a71ae2b8f42e30948
木村太一朗 <kimura@tanosys.com>
2025-07-31 05:04:21 +0900

    スクリプト修正

--- bindingTestApp/clear_and_setup.sh
+++ bindingTestApp/clear_and_setup.sh
@@ -13,6 +13,7 @@ rm -rf hot_loader
 
 # Download installer from GitHub
 echo "Downloading SwiftJsonUI installer..."
+cd bindingTestApp
 curl -fsSL https://raw.githubusercontent.com/Tai-Kimura/SwiftJsonUI/master/installer/bootstrap.sh | bash -s -- -v 6.1.0 --skip-bundle
 
 cd "$SCRIPT_DIR/bindingTestApp/binding_builder/"

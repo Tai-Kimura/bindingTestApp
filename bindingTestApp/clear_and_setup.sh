a334453f79e2a424aaf6c804d82a876e8fa3dc75
木村太一朗 <kimura@tanosys.com>
2025-07-31 00:51:32 +0900

    スクリプト追加

---
+++ bindingTestApp/clear_and_setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Reset git state
+git reset --hard HEAD
+git clean -fd
+
+# Clear SPM cache
+rm -rf ~/Library/Caches/org.swift.swiftpm
+rm -rf .build
+
+cd ~/resource/bindingTestApp/bindingTestApp/bindingTestApp
+cp -r ~/resource/SwiftJsonUI/installer .
+cd installer
+./install_sjui.sh -v 6.1.0-alpha --skip-bundle
+cd ../binding_builder/
+./sjui setup
+./sjui g view splash --root
+./sjui g partial partial_test
+./sjui g partial common/navigation_bar

723094bc2b4bbf849dd7d9f891698c4c5edb8cab
木村太一朗 <kimura@tanosys.com>
2025-08-06 04:41:55 +0900

    スクリプト修正

--- bindingTestApp/clear_and_setup.sh
+++ bindingTestApp/clear_and_setup.sh
@@ -16,13 +16,13 @@ curl -fsSL https://raw.githubusercontent.com/Tai-Kimura/SwiftJsonUI/master/insta
 
 cd "$SCRIPT_DIR/bindingTestApp/"
 ./sjui_tools/bin/sjui setup
-./sjui_tools/bin/sjui g view splash --root
-./sjui_tools/bin/sjui g partial partial_test
-./sjui_tools/bin/sjui g partial common/navigation_bar
-./sjui_tools/bin/sjui g view main
-./sjui_tools/bin/sjui g collection Main/ListItem
+# ./sjui_tools/bin/sjui g view splash --root
+# ./sjui_tools/bin/sjui g partial partial_test
+# ./sjui_tools/bin/sjui g partial common/navigation_bar
+# ./sjui_tools/bin/sjui g view main
+# ./sjui_tools/bin/sjui g collection Main/ListItem
 
-# Start hot loader listener
-echo "Starting hot loader listener..."
-./sjui_tools/bin/sjui hotload listen &
-echo "Hot loader listener started"
+# # Start hot loader listener
+# echo "Starting hot loader listener..."
+# ./sjui_tools/bin/sjui hotload listen &
+# echo "Hot loader listener started"
